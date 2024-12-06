d67eca0b1e0ab210b79e12e86882fbe3f9f6250f
Tedros Habtu <amhopefule07@gmail.com>
2024-12-06 22:15:48 +0300

    Project set up is done 50% mainpage design is over

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+ 
+  return (
+<div className="maincontainer">
+  <div className="location mt-2">
+    <div className="container  border border-1 p-5 ">
+      <div className="d-flex flex-row justify-content-center">
+      <h1>Enter Location</h1>
+      </div>
+     </div>
+  </div>
+
+  <div className="city ">
+    <div className="container  border border-1 p-5 ">
+      <h1>Addis Ababa</h1>
+     </div>
+  </div>
+
+  <div className="bottom ">
+    <div className="container  border border-1 p-5 ">
+      <h1>Addis Ababa</h1>
+     </div>
+  </div>
+     
+
+ </div>
+  )
+}
+
+export default App
